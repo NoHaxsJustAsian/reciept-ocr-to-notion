e19efde02e5302e70e0c3c64783ee0be9f6e2cb4
Win Tongtawee <WinTongtawee@gmail.com>
2024-10-15 22:43:14 -0400

    changed deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
-  base: "/reciept-ocr-to-notion/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
